daa2eb7ea4ed09c54be8c25e597d0746b49e0e52
Richard Kim <emacs18@gmail.com>
2020-07-31 23:47:53 -0700

    updated all pkgs

--- cython-mode-20190111.2150.el
+++ cython-mode-20190111.2150.el
@@ -1,6 +1,6 @@
 ;;; cython-mode.el --- Major mode for editing Cython files
 ;; Package-Version: 20190111.2150
-;; Package-Commit: 5d8527d196c2ffd45b345ae31cac856f0c8fdc07
+;; Package-Commit: b6c0461e4fe08d9c5c91a209c6cd1cc1e70bc05c
 
 ;; License: Apache-2.0
 
--- google-c-style-20180130.1736.el
+++ google-c-style-20180130.1736.el
@@ -2,7 +2,7 @@
 
 ;; Keywords: c, tools
 ;; Package-Version: 20180130.1736
-;; Package-Commit: 718c32728dad5e0f570b7bcf38b619759f06bc4a
+;; Package-Commit: 10748b8973817357ebe3fd1786979c9881f6afd5
 
 ;; google-c-style.el is Copyright (C) 2008 Google Inc. All Rights Reserved.
 ;;
